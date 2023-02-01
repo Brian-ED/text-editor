d5d7ba2efdd69003ebbae07ad188f1319a65ee26
Brian-ED <brianellingsgaard9@gmail.com>
2023-02-01 21:42:29 +0000

    day 3 done! finished autozoom by making a function added saving made newlines possible

--- main.py
+++ main.py
@@ -1,18 +1,47 @@
-from tkinter import * 
+import tkinter as tk
+import asyncio
 font = "BQN386 Unicode"
-fontSize = 20
-lastSize = [0,0]
-lastPos = [0,0]
+minFontSize = 20
+maxFontSize = 200
+root = tk.Tk()
+saveFileName = "saveFile.txt"
 
-root = Tk()
+# justify="left"
+text_box = tk.Text(root, font=(font, maxFontSize))
 
-text_box = Text(root, font=(font,20))
-text_box.pack(expand = True, fill = BOTH)
+text_box.pack(expand = True, fill = 'both')
 
-def resize(event:Event):
-    if (event.x, event.y)==(0, 0):
+
+def onResize(event:tk.Event):
+    pos = event.x, event.y           # pos er x og y positionin av top vinstra punkt av window
+    size = event.width, event.height
+    if pos == (0, 0):
         return
-    print(event)
+    print(pos)
 
-root.bind("<Configure>", resize)
+def onPress(_):
+    text = text_box.get(1.0,'end')
+    with open(saveFileName,'w',encoding="utf-8")as f:
+        f.write(text)
+    
+    longestLine = max(map(len, text.split('\n')))
+
+    # f(charLength) = fontSizeNeeded
+    # f(8) = 200
+    # f(14) = 110
+    # f(54) = 30
+    # f(31) = 50
+    f = lambda x:1550/max(1,x)
+    fontSize = f(longestLine)
+    print(fontSize)
+
+    text_box.configure(font=(font,max(minFontSize, min(maxFontSize, int(fontSize)))))
+
+with open(saveFileName, "r", encoding="utf-8")as f:
+    text_box.insert(1.0, f.read())
+onPress('')
+
+root.bind('<KeyPress>', onPress)
+root.bind("<Configure>", onResize)
+
 root.mainloop()
