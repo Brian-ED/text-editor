48d2e0eb9a144c544ea82027b0aa7ff3019279f5
Brian-ED <brianellingsgaard9@gmail.com>
2023-02-04 01:28:17 +0000

    3rd day, made a ton of progress with buttons, ui made a checklist sorted out code a lot

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-import asyncio
 font = "BQN386 Unicode"
 minFontSize = 20
 maxFontSize = 200
@@ -11,13 +10,14 @@ text_box = tk.Text(root, font=(font, maxFontSize))
 
 text_box.pack(expand = True, fill = 'both')
 
+size = 1284, 701
 
 def onResize(event:tk.Event):
+    global size
     pos = event.x, event.y           # pos er x og y positionin av top vinstra punkt av window
-    size = event.width, event.height
     if pos == (0, 0):
         return
-    print(pos)
+    size = event.width, event.height
 
 def onPress(_):
     text = text_box.get(1.0,'end')
@@ -26,15 +26,19 @@ def onPress(_):
     
     longestLine = max(map(len, text.split('\n')))
 
-    # f(charLength) = fontSizeNeeded
-    # f(8) = 200
-    # f(14) = 110
-    # f(54) = 30
-    # f(31) = 50
-    f = lambda x:1550/max(1,x)
+    # f(charLength, screenWidth) = fontSizeNeeded
+    # f(8, 1284) = 200
+    # f(14,1284) = 110
+    # f(54,1284) = 30
+    # f(31,1284) = 50
+    # f(28,665 ) = 30
+    # f(8, 664 ) = 100
+    # f(15,847 ) = 70
+    f = lambda x:1550/max(1,x)    
+    
+    print(f'{size=}')
     fontSize = f(longestLine)
     print(fontSize)
-
     text_box.configure(font=(font,max(minFontSize, min(maxFontSize, int(fontSize)))))
 
 with open(saveFileName, "r", encoding="utf-8")as f:
